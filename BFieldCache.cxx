d16df2abd308fac81a76bc282d996b5025c04a6a
christos <christos@cern.ch>
2020-09-02 16:49:09 +0200

    update comments

--- BFieldCache.cxx
+++ BFieldCache.cxx
@@ -140,15 +140,13 @@ BFieldCache::getBVec(const double* ATH_RESTRICT xyz,
   CxxUtils::vec<double, 4> field2_z = {
     m_field[0][4], m_field[0][5], m_field[0][6], m_field[0][7]
   };
-
-  // Load Br at 8 corners of the bin
+ // Load Br at 8 corners of the bin
   CxxUtils::vec<double, 4> field1_r = {
     m_field[1][0], m_field[1][1], m_field[1][2], m_field[1][3]
   };
   CxxUtils::vec<double, 4> field2_r = {
     m_field[1][4], m_field[1][5], m_field[1][6], m_field[1][7]
   };
-
   // Load Bphi at 8 corners of the bin
   CxxUtils::vec<double, 4> field1_phi = {
     m_field[2][0], m_field[2][1], m_field[2][2], m_field[2][3]
@@ -157,16 +155,7 @@ BFieldCache::getBVec(const double* ATH_RESTRICT xyz,
     m_field[2][4], m_field[2][5], m_field[2][6], m_field[2][7]
   };
 
-  // Do the 1st stage of the interpolation of Bz,Br,Bphi
-  // inside the z,r,phi bin.
-  //
-  //  We end up with
-  //  3 z,r,phi x size 4 SIMD vectors :
-  //  Each vector entries are
-  //  0 gr * (gphi * field[0] + fphi * field[4]) ,
-  //  1 fr * (gphi * field[1] + fphi * field[5]) ,
-  //  2 gr * (gphi * field[2] + fphi * field[6]) ,
-  //  3 fr * (gphi * field[3] + fphi * field[7]) ,
+
   CxxUtils::vec<double, 4> gPhiM_z = field1_z * gphi;
   CxxUtils::vec<double, 4> fPhiM_z = field2_z * fphi;
   CxxUtils::vec<double, 4> interp_z = (gPhiM_z + fPhiM_z) * rInterCoeff;
@@ -179,9 +168,18 @@ BFieldCache::getBVec(const double* ATH_RESTRICT xyz,
   CxxUtils::vec<double, 4> fPhiM_phi = field2_phi * fphi;
   CxxUtils::vec<double, 4> interp_phi = (gPhiM_phi + fPhiM_phi) * rInterCoeff;
 
+  //  We end up with
+  //  3 (z,r,phi) size 4 SIMD vectors :
+  //  The entries of each of the 3 SIMD vectors are :
+  //  0 :  gr * (gphi * field[0] + fphi * field[4]) ,
+  //  1 : fr * (gphi * field[1] + fphi * field[5]) ,
+  //  2 : gr * (gphi * field[2] + fphi * field[6]) ,
+  //  3 :3 fr * (gphi * field[3] + fphi * field[7]) ,
 
-  //4 simd vector of 3 entries r,z,phi
-  //Since we want to retain also binary compatibility
+
+  //We want to retain also binary compatibility
+  //Switch to 4 vector of 3 entries (z,r,phi)
+
   CxxUtils::vec<double, 4> Bzrphivec0 = {
     interp_z[0], interp_r[0], interp_phi[0], 0
   };
@@ -198,6 +196,7 @@ BFieldCache::getBVec(const double* ATH_RESTRICT xyz,
   //Do the final interpolation ster for all 3 Bz,Br,Bphi
   CxxUtils::vec<double, 4> Bzrphi1 = (Bzrphivec0 + Bzrphivec1) * gz;
   CxxUtils::vec<double, 4> Bzrphi2 = (Bzrphivec2 + Bzrphivec3) * fz;
+  //now create the final (r,z,phi) values
   CxxUtils::vec<double, 4> Bzrphi = (Bzrphi1 + Bzrphi2) * m_scale;
 
   // convert (Bz,Br,Bphi) to (Bx,By,Bz)
