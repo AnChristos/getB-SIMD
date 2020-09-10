22767ea475a74953de6110824fc8294b7248f81a
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2020-09-11 00:02:56 +0100

    script update

--- buildGoogleBenchmark.sh
+++ buildGoogleBenchmark.sh
@@ -1,5 +1,6 @@
 cd benchmark/
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_GTEST_TESTS=OFF -DCMAKE_INSTALL_PREFIX=${HOME}/.local ../ 
+cd ../
 cmake --build "build" --config Release --target install 
 cd ../
