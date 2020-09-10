f133588f99140418a2aabaf5e10336cbc47e44ba
christos <christos@cern.ch>
2020-09-11 00:31:20 +0100

    Update installation and CMakeFiles

--- buildGoogleBenchmark.sh
+++ buildGoogleBenchmark.sh
@@ -1,5 +1,5 @@
 cd benchmark/
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_GTEST_TESTS=OFF -DCMAKE_INSTALL_PREFIX=${HOME}/.local ../ 
-cmake --build "build" --config Release --target install 
+cmake --build --config Release --target install 
 cd ../
